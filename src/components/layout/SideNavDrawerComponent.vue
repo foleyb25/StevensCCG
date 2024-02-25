130273f1256b36b405d27e7a1464c6a363646557
foleyb25 <foleyb25@gmail.com>
2024-02-25 15:19:57 -0700

    Update side nav coloring on mobile

--- src/components/layout/SideNavDrawerComponent.vue
+++ src/components/layout/SideNavDrawerComponent.vue
@@ -1,27 +1,25 @@
 <template>
   <!-- drawer component -->
   <div
-    class="fixed z-40 h-screen p-4 overflow-y-auto bg-white w-80 max-w-full dark:bg-gray-800 font-din"
+    class="fixed z-40 h-screen p-4 overflow-y-auto bg-black text-white w-80 max-w-full dark:bg-gray-800 font-din"
   >
-    <h5
-      class="text-base font-semibold text-gray-500 uppercase dark:text-gray-400"
-    >
+    <h5 class="text-base font-semibold text-white uppercase">
       Stevens Custom Crushing and Gravel
     </h5>
     <button
       type="button"
-      class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 absolute top-2.5 right-2.5 inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
+      class="text-white hover:bg-gray-200 hover:text-gray-900 bg-transparent rounded-lg text-sm p-1.5 absolute top-2.5 right-2.5 inline-flex items-center"
       @click="$emit('toggleDrawer')"
     >
       X
       <span class="sr-only">Close menu</span>
     </button>
     <div class="py-4 overflow-y-auto">
-      <ul class="space-y-2">
+      <ul class="space-y-2 text-white">
         <li>
           <router-link to="/">
             <button
-              class="flex items-center p-2 w-full text-base text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+              class="flex items-center p-2 w-full text-base text-white hover:bg-gray-200 hover:text-gray-900 rounded-lg transition duration-75 group"
             >
               <img :src="homeIcon" class="w-8 h-8" alt="" />
               <span class="flex-1 ml-3 text-left whitespace-nowrap">Home</span>
@@ -31,7 +29,7 @@
         <li>
           <router-link to="/services">
             <button
-              class="flex items-center p-2 w-full text-base text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+              class="flex items-center p-2 w-full text-base text-white hover:bg-gray-200 hover:text-gray-900 rounded-lg transition duration-75 group"
             >
               <img :src="servicesIcon" class="w-8 h-8" alt="" />
               <span class="flex-1 ml-3 text-left whitespace-nowrap"
@@ -43,7 +41,7 @@
         <li>
           <router-link to="/about">
             <button
-              class="flex items-center p-2 w-full text-base text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+              class="flex items-center p-2 w-full text-base text-white hover:bg-gray-200 hover:text-gray-900 rounded-lg transition duration-75 group"
             >
               <img :src="aboutIcon" class="w-8 h-8" alt="" />
               <span
@@ -57,9 +55,13 @@
         <li>
           <router-link to="/projects">
             <button
-              class="flex items-center p-2 w-full text-base text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+              class="flex items-center p-2 w-full text-base text-white hover:bg-gray-200 hover:text-gray-900 rounded-lg transition duration-75 group"
             >
-              <img :src="projectsIcon" class="w-8 h-8" alt="" />
+              <img
+                :src="projectsIcon"
+                class="w-8 h-8 filter grayscale brightness-100"
+                alt=""
+              />
               <span
                 class="flex-1 ml-3 text-left whitespace-nowrap"
                 sidebar-toggle-item
