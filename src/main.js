216e78acb688b597b74f9834a8824921c200435e
foleyb25 <foleyb25@gmail.com>
2024-06-21 17:50:07 -0600

    Fixing minor syntax issue in main.js

--- src/main.js
+++ src/main.js
@@ -3,7 +3,10 @@ import "./style.css";
 import App from "./App.vue";
 import router from "./router/index.js";
 import { faCheckCircle } from "@fortawesome/free-regular-svg-icons";
-import { faBars, faCircleChevronRight } from "@fortawesome/free-solid-svg-icons";
+import {
+  faBars,
+  faCircleChevronRight,
+} from "@fortawesome/free-solid-svg-icons";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
